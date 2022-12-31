a4089c9dff65a14cc53f6ef67157c7684d0cb44e
JustinAchanzar <justinachanzar@gmail.com>
2022-12-31 14:40:10 -0800

    Worskhop 4

---
+++ routes/test.js
@@ -0,0 +1,9 @@
+if ('favorite document for user exists in database') {
+    if ('the id in my route param is NOT in the database array') {
+        'push it in!'
+    } else {
+        'tell the user it is already there'
+    }
+} else {
+    'make a new favorite collection for the user'
+}
